680986eb5ca3acd94e18a79d04cf1821ad70881c
Pedro Vanzella <pedro@pedrovanzella.com>
2011-06-02 20:50:39 -0300

    lógica do servidor semi-documentada

--- main.c
+++ main.c
@@ -22,6 +22,24 @@ struct aviao* voos[50];
 int main(int argc, char* argv[])
 {
 
+  /* MODO CLIENTE */
+  /* MODO SERVIDOR */
+  /* Pede por login */
+  if(login(user, pass))
+  {
+    /* Usuario logado */ 
+    /* imprime prompt de comando */
+    char cmd;
+    /* grande case de opcoes */
+
+    switch(cmd)
+    {
+      case 'c': /* Chunky */
+        break;
+      case 'b': /* Bacon */
+        break;
+    }
+  }
 
   return 0;
 }
