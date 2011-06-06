f8a401ca36744d172682a4d26cd002805b91b81b
Pedro Vanzella <pedro@pedrovanzella.com>
2011-06-06 17:37:42 -0300

    cast no malloc

--- usuario.c
+++ usuario.c
@@ -18,11 +18,11 @@ struct usuario* novo_usuario(char* nome, char* senha)
 struct usuario* login(char* nome, char* senha)
 {
   struct usuario* user;
-  *user = malloc(sizeof(struct usuario));
+  user = (struct usuario*) malloc(sizeof(struct usuario));
   user = find_by_name(nome);
   if(!user)
   {
-    free(*user); // Memory leak
+    free(user); // Memory leak
     return NULL; // Se user nao existe
   }
   if(!(strcmp(user->senha, senha))) return user; // Se a senha esta certa
