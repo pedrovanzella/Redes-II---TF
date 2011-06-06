039e67d9a124b370df38f4abaf43bd16208c110b
Pedro Vanzella <pedro@pedrovanzella.com>
2011-06-06 17:33:53 -0300

    funcao de login. Favor testar

--- usuario.c
+++ usuario.c
@@ -17,7 +17,10 @@ struct usuario* novo_usuario(char* nome, char* senha)
 
 struct usuario* login(char* nome, char* senha)
 {
-
+  struct usuario* user = find_by_name(nome);
+  if(!user) return NULL; // Se user nao existe
+  if(!(strcmp(user->senha, senha))) return user; // Se a senha esta certa
+  return NULL; // Senha errada
 }
 
 struct usuario* find_by_name(char* name)
