21ba544b40e902314aa4710bb46d6e7cd27c7fc9
Pedro Vanzella <pedro@pedrovanzella.com>
2011-06-12 18:02:09 -0300

    limpando a logica

--- main.c
+++ main.c
@@ -53,35 +53,13 @@ int main(int argc, char* argv[])
     scanf("%s", usr->senha);
   }
 
-  if(*argv[1] == 's')
+  else if(*argv[1] == 's')
   {
-//	Servidor();
-//	  return;  
     /* MODO SERVIDOR */
     popula_db_users();
     popula_db_voos();
-    /* inicia banco do servidor, lendo arquivo de avioes */
-       /* Pede por login */
-    char user[20];
-    char pass[20];
-    strcpy(user, "abs");
-    strcpy(pass, "blz");
-    if(login(user, pass))
-    {
-      /* Usuario logado */ 
-      /* imprime prompt de comando */
-      char cmd;
-      /* grande case de opcoes */
-
-      switch(cmd)
-      {
-        case 'c': /* Chunky */
-          break;
-        case 'b': /* Bacon */
-          break;
-      }
-    }
   }
+
   else /* parametro errado */
   {
     fprintf(stderr, "Parametro desconhecido (%c). Parametros suportados sao s para servidor ou c para cliente\n", *argv[1]);
